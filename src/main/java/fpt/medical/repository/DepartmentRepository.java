1befcb4a60bb3b9c3e6588d8074e0b397c09c598
dachello <danhdac2k5@gmail.com>
2026-07-01 11:12:27 +0700

    Code Department

--- src/main/java/fpt/medical/repository/DepartmentRepository.java
+++ src/main/java/fpt/medical/repository/DepartmentRepository.java
@@ -1,9 +1,14 @@
 package fpt.medical.repository;
 
 import fpt.medical.entity.Department;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
+    Page<Department> findByNameContainingIgnoreCase(String keyword, Pageable pageable);
+    boolean existsByNameIgnoreCase(String name);
+    boolean existsByNameIgnoreCaseAndIdNot(String name, Long id);
 }
