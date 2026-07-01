70fbc1e13d667348cae994c6321afa6dce788aec
yennhhe187368 <yennhhe187368@fpt.edu.vn>
2026-07-01 20:01:59 +0700

    yến tạo doctor list lần 1

--- src/main/java/fpt/medical/controller/PatientController.java
+++ src/main/java/fpt/medical/controller/PatientController.java
@@ -2,10 +2,16 @@ package fpt.medical.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/patients")
 @RequiredArgsConstructor
 public class PatientController {
+
+    @GetMapping("/doctors")
+    public String doctorList() {
+        return "patient/doctor-list";
+    }
 }
